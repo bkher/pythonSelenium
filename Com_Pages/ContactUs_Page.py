1eeef824aa9cc57a5ebbb1e4970a632883003a4e
Bhagi <Bhagi@Bhagi-PC>
2019-02-03 20:23:42 +0530

    ContactUsPage Changes

--- Com_Pages/ContactUs_Page.py
+++ Com_Pages/ContactUs_Page.py
@@ -10,17 +10,27 @@ class contactUsPage:
         self.Order_link_xpath = locators.Order_link_xpath
 
     def helpcentertext(self):
-        print ('help Center text is displayed = ',self.driver.find_element_by_xpath(self.helpcenter_Text_Xpath).is_displayed())
+        ele = self.driver.find_element_by_xpath(self.helpcenter_Text_Xpath).text
+        assert ele == "HELP CENTER"
+        #print ('help Center text is displayed = ',self.driver.find_element_by_xpath(self.helpcenter_Text_Xpath).is_displayed())
         
     def orderrelatedQuesryLink(self):
-        print ('order related queries link is enable= ',self.driver.find_element_by_xpath(self.OrderRelatedQuery_link_xpath).is_enabled())
+        ele = self.driver.find_element_by_xpath(self.OrderRelatedQuery_link_xpath).is_enabled()
+        assert ele == True
+        #print ('order related queries link is enable= ',self.driver.find_element_by_xpath(self.OrderRelatedQuery_link_xpath).is_enabled())
 
     def NonOrderRelatedQuesry(self):
-        print('Non order related query is enable= ',self.driver.find_element_by_xpath(self.NonOrderRelatedIssue_link_xpath).is_enabled())
+        ele = self.driver.find_element_by_xpath(self.NonOrderRelatedIssue_link_xpath).is_enabled()
+        assert ele ==True
+        #print('Non order related query is enable= ',self.driver.find_element_by_xpath(self.NonOrderRelatedIssue_link_xpath).is_enabled())
 
     def FrequentlyAsledQuestions(self):
-        print('frequently asked question is displayed=  ',self.driver.find_element_by_xpath(self.FrequentlyAskedQuestons_link_Xpat).is_displayed())
+        ele = self.driver.find_element_by_xpath(self.FrequentlyAskedQuestons_link_Xpat).is_displayed()
+        assert ele == True
+        #print('frequently asked question is displayed=  ',self.driver.find_element_by_xpath(self.FrequentlyAskedQuestons_link_Xpat).is_displayed())
 
     def Order_button(self):
-        print('order button is enable=  ',self.driver.find_element_by_xpath(self.Order_link_xpath).is_enabled())
+        ele = self.driver.find_element_by_xpath(self.Order_link_xpath).is_enabled()
+        assert ele == True
+        #print('order button is enable=  ',self.driver.find_element_by_xpath(self.Order_link_xpath).is_enabled())
         
